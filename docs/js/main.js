0c62522c013ab0d7af74b672a50776214306e7d7
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2023-02-25 17:13:30 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,20 @@
 $(function () {
 
 	var mixer = mixitup('.directions__list');
+})
 
 $('.slider__team').slick({
 	arrows: false,
 	slidesToShow: 4,
+	draggable: false,
 })
 
+$('.team__slider-prev').on('click', function (e) {
+	e.preventDefault()
+	$('.slider__team').slick('slickPrev')
+	})
+$('.team__slider-next').on('click', function (e) {
+	e.preventDefault()
+	$('.slider__team').slick('slickNext')
+	})
 
-})
