85f1c18c822ee596e2fb633bbb99bd9ef77612d4
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2023-03-06 17:31:36 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,6 +10,31 @@ $('.slider__team').slick({
 	dots: true,
 	variableWidth: true,
 	autoplaySpeed: 2000,
+	responsive:
+		[
+			{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 3,
+					variableWidth: false,
+				},
+			},
+			{
+				breakpoint: 965,
+				settings: {
+					slidesToShow: 2,
+					variableWidth: true,
+
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					variableWidth: false,
+				},
+			},
+		]
 })
 
 $('.team__slider-prev').on('click', function (e) {
@@ -26,7 +51,17 @@ $('.testimonials__slider').slick({
 	arrows: false,
 	dots: true,
 	dotsClass: 'dots-testimonials',
+	appendDots: $('.dots-testimonials'),
+	responsive:
+		[
+			{
+				breakpoint: 700,
+				settings: {
+				},
+			},
+		]
 })
+
 
 $('.testimonials-prev').on('click', function (e) {
 	e.preventDefault()
@@ -56,3 +91,35 @@ $('.acc__item').on('click', function (e) {
 	}
 })
 
+
+$("#menu, .header__nav, .footer__links, .footer__bottom-inner, .header__nav-box, .header__buttons").on("click", "a", function (e) {
+
+	//отменяем стандартную обработку нажатия по ссылке
+	e.preventDefault();
+	//забираем идентификатор бока с атрибута href
+	var id = $(this).attr('href'),
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+		top = $(id).offset().top;
+	//анимируем переход на расстояние - top за 1500 мс
+	$('body,html').animate({ scrollTop: top }, 1000);
+})
+
+setInterval(() => {
+	if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+		$('.burger').addClass('burger--follow')
+	} else {
+		$('.burger').removeClass('burger--follow')
+	}
+})
+
+$('.burger, .overlay').on('click', function (e) {
+	e.preventDefault();
+	$('.header__top').toggleClass('header__top--open')
+	$('.overlay').toggleClass('overlay--show')
+	$('.burger').toggleClass('active')
+})
+
+
+$('.footer__title-slide').on('click', function () {
+	$(this).next().slideToggle("slow")
+})
