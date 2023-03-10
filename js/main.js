a37094463cc2a720bc13de2a05435fd0faf5b285
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2023-03-10 12:56:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // конечная дата, например 1 июля 2021
+  const deadline = new Date(2023, 12, 31);
+  // id таймера
+  let timerId = null;
+  // склонение числительных
+  function declensionNum(num, words) {
+    return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+  }
+  // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+  function countdownTimer() {
+    const diff = deadline - new Date();
+    if (diff <= 0) {
+      clearInterval(timerId);
+    }
+    const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+    const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+    const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+
+    $days.textContent = days < 10 ? '0' + days : days;
+    $hours.textContent = hours < 10 ? '0' + hours : hours;
+    $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+
+    $days.dataset.title = declensionNum();
+    $hours.dataset.title = declensionNum();
+    $minutes.dataset.title = declensionNum();
+
+  }
+  // получаем элементы, содержащие компоненты даты
+  const $days = document.querySelector('.timer__days');
+  const $hours = document.querySelector('.timer__hours');
+  const $minutes = document.querySelector('.timer__minutes');
+
+  // вызываем функцию countdownTimer
+  countdownTimer();
+  // вызываем функцию countdownTimer каждую секунду
+  timerId = setInterval(countdownTimer, 1000);
+	
+	$(window).scroll(function(){
+		var sticky = $('.header__menu'),
+				scroll = $(window).scrollTop();
+	
+		if (scroll >= 100) sticky.addClass('fixed');
+		else sticky.removeClass('fixed');
+	});
+});
