de8bc71653770aa57be7b367135fda95a1004824
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2023-03-11 18:44:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $('.reviews__slider').slick({
   speed: 700,
   slidesToShow: 4,
   slidesToScroll: 1,
-	
   responsive: [
     {
       breakpoint: 1024,
@@ -35,13 +34,59 @@ $('.reviews__slider').slick({
   ]
 });
 
-$('.team__slider-prev').on('click', function (e) {
+$('.reviews__slider-prev').on('click', function (e) {
 	e.preventDefault()
 	$('.reviews__slider').slick('slickPrev')
 })
-$('.team__slider-next').on('click', function (e) {
+$('.reviews__slider-next').on('click', function (e) {
 	e.preventDefault()
 	$('.reviews__slider').slick('slickNext')
+})
+
+
+$('.order__slider').slick({
+  dots: true,
+	arrows: false,
+  speed: 700,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.order__slider-prev').on('click', function (e) {
+	e.preventDefault()
+	$('.order__slider').slick('slickPrev')
+})
+$('.order__slider-next').on('click', function (e) {
+	e.preventDefault()
+	$('.order__slider').slick('slickNext')
 })
 
 
