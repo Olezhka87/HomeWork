760b97d32e5981f2bde0791b7a96114891bf9106
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2023-03-12 18:55:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -89,6 +89,50 @@ $('.order__slider-next').on('click', function (e) {
 	$('.order__slider').slick('slickNext')
 })
 
+$('.gift__slider').slick({
+  dots: true,
+	arrows: false,
+  speed: 700,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.gift__slider-prev').on('click', function (e) {
+	e.preventDefault()
+	$('.gift__slider').slick('slickPrev')
+})
+$('.gift__slider-next').on('click', function (e) {
+	e.preventDefault()
+	$('.gift__slider').slick('slickNext')
+})
 
 
 
