8f9f879a3f538504e7e3869df14a8b27c3d71b05
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2023-03-14 21:44:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -179,6 +179,51 @@ $('.sert__slider-next').on('click', function (e) {
 	$('.sert__slider').slick('slickNext')
 })
 
+$('.contact__slider').slick({
+  dots: true,
+	arrows: false,
+  speed: 700,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.contact__slider-prev').on('click', function (e) {
+	e.preventDefault()
+	$('.contact__slider').slick('slickPrev')
+})
+$('.contact__slider-next').on('click', function (e) {
+	e.preventDefault()
+	$('.contact__slider').slick('slickNext')
+})
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -235,3 +280,4 @@ function isNumberKey(evt) {
 		return false;
 	return true;
 }
+
